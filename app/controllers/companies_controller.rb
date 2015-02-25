a32b501e832c356743d074b9dec9c4ce935e1146
Nick Kalinoski <nickkalinoski@Nicks-MacBook-Air.local>
2015-02-25 14:53:02 -0700

    create company successfully

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -15,20 +15,18 @@ class CompaniesController < ApplicationController
   def create
     @company = Company.new(company_params)
 
-    respond_to do |format|
-      if @company.save
-        redirect_to @company, notice: "Company Successfully Created!"
-      else
-        render :new
-      end
+    if @company.save
+      redirect_to @company, notice: "Company Successfully Created!"
+    else
+      render :new
     end
   end
 
 
-private
+  private
 
-def company_params
-  params.require(:company).permit(:name, :catch_phrase, :suffix)
+  def company_params
+    params.require(:company).permit(:name, :catch_phrase, :suffix)
 
-end
+  end
 end
