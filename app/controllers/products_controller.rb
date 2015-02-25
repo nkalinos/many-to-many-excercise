e1cedb6979746bbeecd47bb8789c95f1683ccd6f
Nick Kalinoski <nickkalinoski@Nicks-MacBook-Air.local>
2015-02-25 14:32:06 -0700

    add product link and show page for individual product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,19 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+end
 
     def create
 
       ### create a product here.
     end
+
+private
+
+def product_params
+  params.require(:product).permit(:name, :price, :company_id)
+end
 
 end
