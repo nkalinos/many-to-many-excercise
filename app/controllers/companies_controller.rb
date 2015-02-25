04f5f41baf79229182fdb8e9a24c34d5c93af8bc
Nick Kalinoski <nickkalinoski@Nicks-MacBook-Air.local>
2015-02-25 14:04:03 -0700

    add link to companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,16 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def show
+    @company = Company.find(params[:id])
+  end
+
+
+
+private
+
+def company_params
+  params.require(:company).permit(:name, :catch_phrase, :suffix)
+
+end
 end
