dadd1f5a84c1369318f132b4cce89a644d7bd5fc
Nick Kalinoski <nickkalinoski@Nicks-MacBook-Air.local>
2015-02-25 14:49:13 -0700

    add new company form page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,6 +8,21 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    respond_to do |format|
+      if @company.save
+        redirect_to @company, notice: "Company Successfully Created!"
+      else
+        render :new
+      end
+    end
+  end
 
 
 private
